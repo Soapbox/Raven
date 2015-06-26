ce3d04cf7b8447c8745e0109d97797bba3fb7bff
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-26 15:29:19 -0400

    Fix automatic configurations

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Create a sites-available directory
+mkdir /etc/httpd/sites-available 2>/dev/null
+mkdir /etc/httpd/sites-enabled 2>/dev/null
+
+# Create a logs directory
 mkdir /var/logs/$1 2>/dev/null
+
+# Create a ssl directory
 mkdir /etc/ssl 2>/dev/null
+
+# Generate our ssl keys
 openssl genrsa -out "/etc/ssl/$1.key" 1024 2>/dev/null
 openssl req -new -key /etc/ssl/$1.key -out /etc/ssl/$1.csr -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
 openssl x509 -req -days 365 -in /etc/ssl/$1.csr -signkey /etc/ssl/$1.key -out /etc/ssl/$1.crt 2>/dev/null
@@ -11,8 +20,8 @@ block="
         ServerName   "$1"
         DocumentRoot "$2"
 
-        ErrorLog     "/var/logs/$1/error.log"
-        CustomLog    "/var/logs/$1/access.log" combined
+        ErrorLog     "/etc/httpd/logs/$1/error.log"
+        CustomLog    "/etc/httpd/logs/$1/access.log" combined
     </VirtualHost>
     <VirtualHost $1:443>
             SSLEngine on
@@ -24,12 +33,12 @@ block="
             ServerName      "$1"
             DocumentRoot    "$2"
 
-            CustomLog       "/var/logs/$1/access.log" combined
-            ErrorLog        "/var/logs/$1/error.log"
+            CustomLog       "/etc/httpd/logs/$1/access.log" combined
+            ErrorLog        "/etc/httpd/logs/$1/error.log"
     </VirtualHost>
 "
 
-echo "$block" > "/etc/apache2/sites-available/$1.conf"
-ln -fs "/etc/apache2/sites-available/$1" "/etc/apache2/sites-enabled/$1"
+echo "$block" > "/etc/httpd/sites-available/$1.conf"
+ln -fs "/etc/httpd/sites-available/$1.conf" "/etc/httpd/sites-enabled/$1.conf"
 
-sudo a2ensite $1.conf
+#service httpd reload
