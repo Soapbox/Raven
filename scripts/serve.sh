e7c6c6a307d1556d462b84484ac13a8eedb5fb56
Jaspaul Bola <jaspaul.b@gmail.com>
2015-06-29 13:28:20 -0400

    Simplify serve.sh so it works

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -15,28 +15,13 @@ openssl genrsa -out "/etc/ssl/$1.key" 1024 2>/dev/null
 openssl req -new -key /etc/ssl/$1.key -out /etc/ssl/$1.csr -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
 openssl x509 -req -days 365 -in /etc/ssl/$1.csr -signkey /etc/ssl/$1.key -out /etc/ssl/$1.crt 2>/dev/null
 
-block="
-    <VirtualHost $1:80>
-        ServerName   "$1"
-        DocumentRoot "$2"
-
-        ErrorLog     "/etc/httpd/logs/$1/error.log"
-        CustomLog    "/etc/httpd/logs/$1/access.log" combined
-    </VirtualHost>
-    <VirtualHost $1:443>
-            SSLEngine on
-            SSLCipherSuite ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP
-
-            SSLCertificateFile      /etc/ssl/$1.crt
-            SSLCertificateKeyFile   /etc/ssl/$1.key
-
-            ServerName      "$1"
-            DocumentRoot    "$2"
-
-            CustomLog       "/etc/httpd/logs/$1/access.log" combined
-            ErrorLog        "/etc/httpd/logs/$1/error.log"
-    </VirtualHost>
-"
+block="<Directory \"$2\">
+    AllowOverride All
+</Directory>
+<VirtualHost *:80>
+    ServerName   \"$1\"
+    DocumentRoot \"$2\"
+</VirtualHost>"
 
 echo "$block" > "/etc/httpd/sites-available/$1.conf"
 ln -fs "/etc/httpd/sites-available/$1.conf" "/etc/httpd/sites-enabled/$1.conf"
