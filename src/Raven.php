127c1c9677be2913ea9eb8dfeb02525e619f3c23
Justin Hayes <justin@soapboxhq.com>
2015-07-15 17:40:57 -0400

    Added checking for updates before executing commands

--- src/Raven.php
+++ src/Raven.php
@@ -1,19 +1,32 @@
 <?php namespace SoapBox\Raven;
 
+use KevinGH\Version\Version;
 use SoapBox\Raven\Commands;
+use SoapBox\Raven\Utils\RavenStorage;
+use SoapBox\Raven\Utils\SelfUpdater;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Raven extends Application {
+	private $selfUpdateCommand;
+	private $storage;
+
 	public function __construct($name = 'Raven', $version = '@version@')
 	{
 		parent::__construct($name, $version);
 		$this->registerCommands();
+
+		$this->storage = RavenStorage::getStorage();
 	}
 
 	private function registerCommands()
 	{
+		$this->selfUpdateCommand = new Commands\SelfUpdateCommand;
+		$this->add($this->selfUpdateCommand);
+
 		$this->add(new Commands\ClearCacheCommand);
 		$this->add(new Commands\DestroyCommand);
 		$this->add(new Commands\EditCommand);
@@ -25,16 +38,63 @@ class Raven extends Application {
 		$this->add(new Commands\RefreshCommand);
 		$this->add(new Commands\ResumeCommand);
 		$this->add(new Commands\RunCommand);
-		$this->add(new Commands\SelfUpdateCommand);
 		$this->add(new Commands\SshCommand);
 		$this->add(new Commands\StatusCommand);
 		$this->add(new Commands\SuspendCommand);
 		$this->add(new Commands\UpCommand);
 		$this->add(new Commands\UpdateCommand);
 	}
+
+	/**
+	 * Check to see if the current version of Raven is out of date
+	 */
+	private function isOutdated() {
+		$currentVersion = new Version($this->getVersion());
+		$latestVersion = new Version($this->storage->get('latest_version'));
+		if ($latestVersion->isGreaterThan($currentVersion)) {
+			return true;
+		}
+
+		$lastUpdateCheck = (int) $this->storage->get('last_update_check');
+
+		if (time() - $lastUpdateCheck < 60 * 60 * 8 && $lastUpdateCheck > 0) {
+			return false;
+		}
+
+		$this->storage->set('last_update_check', time());
+
+		$selfUpdater = new SelfUpdater($this);
+		$update = $selfUpdater->getUpdate();
+
+		if ( !is_null($update) && $update->isNewer($selfUpdater->getVersion()) ) {
+			$this->storage->set('latest_version', (string) $update->getVersion());
+			return true;
+		}
+
+		return false;
+	}
 	
 	public function run(InputInterface $input = null, OutputInterface $output = null)
 	{
+		if (null === $input) {
+            $input = new ArgvInput();
+        }
+
+        if (null === $output) {
+            $output = new ConsoleOutput();
+        }
+
+        $this->configureIO($input, $output);
+
+        $command = $this->getCommandName($input);
+
+		if ($this->isOutdated() && $command != $this->selfUpdateCommand->getName()) {
+			$output->writeln(sprintf("<fg=black;bg=yellow>There is a newer version of %s available. Run %s to update.</fg=black;bg=yellow>\n",
+				$this->getName(),
+				$this->selfUpdateCommand->getName()
+			));
+		}
+
 		return parent::run($input, $output);
 	}
 }
