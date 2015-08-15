e6ed7eac2cef0bb9ba66fa7a2a81727be05dc541
Justin Hayes <justin@soapboxhq.com>
2015-08-14 21:51:36 -0400

    Added autoloading for commands.

--- src/Utils/DispatcherCommand.php
+++ src/Utils/DispatcherCommand.php
@@ -1,8 +1,10 @@
 <?php namespace SoapBox\Raven\Utils;
 
+use ReflectionClass;
 use RuntimeException;
 use SoapBox\Raven\Utils\Command;
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
@@ -13,13 +15,20 @@ abstract class DispatcherCommand extends Command {
 	private $argv;
 
 	public function __construct($name = null)
-    {
-    	parent::__construct($name);
+	{
+		parent::__construct($name);
 
-    	$this->registerCommands();
-        $this->generateCommandList();
-    }
+		$this->registerCommands();
+		$this->generateCommandList();
+	}
 
+	public function getSynopsis($bool = false) {
+		return sprintf('%s command [options] [arguments]', $this->getName());
+	}
+
+	/**
+	 * This method generated the available command list for the help output.
+	 */
 	private function generateCommandList() {
 		$section = $this->makeSection('Available commands');
 
@@ -44,7 +53,7 @@ abstract class DispatcherCommand extends Command {
 	/**
 	 * Set up this object based on the command's input
 	 *
-	 * @var Symfony\Component\Console\Input\InputInterface $input The command's input
+	 * @param Symfony\Component\Console\Input\InputInterface $input The command's input
 	 */
 	private function setUp(InputInterface $input) {
 		$argv = $input->getArgument('args');
@@ -57,7 +66,7 @@ abstract class DispatcherCommand extends Command {
 	/**
 	 * Get the command to run base on the input
 	 *
-	 * @var Symfony\Component\Console\Input\InputInterface $input The command's input
+	 * @param Symfony\Component\Console\Input\InputInterface $input The command's input
 	 * @return Symfony\Component\Console\Command\Command
 	 * @throws RuntimeException
 	 */
@@ -71,6 +80,7 @@ abstract class DispatcherCommand extends Command {
 				$class = get_class($this);
 				$command = new $class;
 				$command->setApplication($this->getApplication());
+				$command->setDefinition(new InputDefinition());
 			}
 
 			$helpCommand->setCommand($command);
@@ -85,7 +95,7 @@ abstract class DispatcherCommand extends Command {
 	/**
 	 * Find a command registered to this command by its name
 	 *
-	 * @var string $command The name of the command to find
+	 * @param string $command The name of the command to find
 	 * @return Symfony\Component\Console\Command\Command
 	 * @throws RuntimeException
 	 */
@@ -109,33 +119,69 @@ abstract class DispatcherCommand extends Command {
 	/**
 	 * This function is used to register commands with this dispatcher
 	 */
-	abstract protected function registerCommands();
+	private function registerCommands() {
+		$classInfo = new ReflectionClass($this);
+		$dir = dirname($classInfo->getFileName()) . '/' . ucfirst($this->getName());
+		$namespace = $classInfo->getNamespaceName();
+		$files = scandir($dir);
+
+		foreach ($files as $file) {
+			$class = sprintf('%s\%s\%s', $namespace, ucfirst($this->getName()), rtrim($file, '.php'));
+			$this->addCommand(new $class);
+		}
+	}
 
 	/**
 	 * Register a command with this dispatcher
 	 *
-	 * @var Symfony\Component\Console\Command\Command $command The command to register
+	 * @param Symfony\Component\Console\Command\Command $command The command to register
 	 */
-	protected function addCommand(SymfonyCommand $command) {
+	private function addCommand(SymfonyCommand $command) {
 		$this->commands[$command->getName()] = $command;
 		$command->setApplication($this->getApplication());
 	}
 
 	/**
+	 * This function is called before the command is dispatched. This allows you to hook into the dispatcher
+	 * before the command is run. If this function returns a non-zero number, the command will not run and
+	 * the exit status will be the number returned from this function.
+	 *
+	 * @param Symfony\Component\Console\Command\Command $command The command to be executed
+	 * @param Symfony\Component\Console\Input\InputInterface $input The command's input
+	 * @return int 0 or an exit status
+	 */
+	protected function beforeDispatch(SymfonyCommand $command, InputInterface $input) {}
+
+	/**
+	 * This function is called after the command has executed. This allows you to hook into the dispatcher
+	 * after the command get dispatched. The input into this function is the exit status from the dispatched
+	 * command.
+	 *
+	 * @param int $exitStatus The exit status of the dispatched command
+	 */
+	protected function afterDispatch() {}
+
+	/**
 	 * Execute the current command
 	 *
-	 * @var Symfony\Component\Console\Input\InputInterface $input The command's input
-	 * @var Symfony\Component\Console\Output\OutputInterface $output The output for the command
+	 * @param Symfony\Component\Console\Input\InputInterface $input The command's input
+	 * @param Symfony\Component\Console\Output\OutputInterface $output The output for the command
 	 */
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
 		$this->setUp($input);
 
-		if (is_null($this->commandString)) {
-			$this->getApplication()->get('help');
-		}
 		$command = $this->getCommand($input);
+
+		if ($exitStatus = $this->beforeDispatch($command, $input)) {
+			return $exitStatus;
+		}
+
 		$arrayInput = new ArgvInput($this->argv, $command->getDefinition());
-		$command->run($arrayInput, $output);
+		$exitStatus = $command->run($arrayInput, $output);
+
+		$this->afterDispatch($exitStatus);
+
+		return $exitStatus;
 	}
 }
