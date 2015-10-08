e0fc92fbd1a7c58967b8e9a9c4b7c56e8ff5b192
Justin Hayes <justin@soapboxhq.com>
2015-10-08 16:51:19 -0400

    Made watch log tail the entire file initially

--- src/Commands/WatchLogCommand.php
+++ src/Commands/WatchLogCommand.php
@@ -5,20 +5,23 @@ use SoapBox\Raven\Utils\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class WatchLogCommand extends Command {
+class WatchLogCommand extends Command
+{
 	protected $command = 'watch-log';
 	protected $description = 'Tail the latest log file.';
 	private $vagrant;
 
-	protected function addArguments() {
-    $this->makeArgument('path')
-      ->setDescription('The path to the log file to watch')
-      ->setDefault('app/storage/logs');
-  }
+	protected function addArguments()
+	{
+		$this->makeArgument('path')
+			->setDescription('The path to the log file to watch')
+			->setDefault('app/storage/logs');
+	}
 
 	protected function addOptions() {}
 
-	public function execute(InputInterface $input, OutputInterface $output) {
-    system('cd ' . $input->getArgument('path') . ' && tail -f `ls -t | head -1`');
+	public function execute(InputInterface $input, OutputInterface $output)
+	{
+		system('cd ' . $input->getArgument('path') . ' && tail -f -n `ls -t | head -1 | xargs cat | wc -l` `ls -t | head -1`');
 	}
 }
