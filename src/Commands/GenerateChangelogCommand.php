8add48883a74b2839d8ee62a3ac90237eb8878d2
Justin Hayes <justin@soapboxhq.com>
2015-10-09 00:40:30 -0400

    Added access token generation

--- src/Commands/GenerateChangelogCommand.php
+++ src/Commands/GenerateChangelogCommand.php
@@ -3,9 +3,11 @@
 use RuntimeException;
 use GuzzleHttp\Client;
 use SoapBox\Raven\Utils\Command;
+use SoapBox\Raven\Utils\RavenStorage;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class GenerateChangelogCommand extends Command {
@@ -28,44 +30,85 @@ class GenerateChangelogCommand extends Command {
 		return $output;
 	}
 
-	public function execute(InputInterface $input, OutputInterface $output)
-	{
-		$output = $this->exec('git config --get remote.origin.url');
+	private function getAccessToken(Client $client) {
+		$storage = RavenStorage::getStorage();
 
-		$matches = [];
-		if ( !preg_match('/^git@github\.com\:(?P<owner>.+)\/(?P<repo>.+)\.git$/', $output[0], $matches) ) {
-			throw new RuntimeException('Cannot find a remote git repository.');
+		if (($accessToken = $storage->get('github_access_token')) === '') {
+			$email = $this->exec('git config --global user.email')[0];
+
+			$question = new Question(sprintf('Enter host password for user \'%s\':', $email));
+		    $question->setHidden(true);
+		    $question->setHiddenFallback(false);
+
+		    $helper = $this->getHelper('question');
+		    $password = $helper->ask($input, $output, $question);
+
+		    $params = [
+		    	"scopes" => [
+		    		"repo"
+		    	],
+		    	"note" => "Raven"
+		    ];
+
+			$response = $client->post('/authorizations', [
+				'auth' => [$email, $password],
+				'body' => json_encode($params)
+			]);
+
+			$accessToken = json_decode($response->getBody())->token;
+			$storage->set('github_access_token', $accessToken);
 		}
 
+		return $accessToken;
+	}
+
+	private function getReleaseTags(InputInterface $input) {
 		$tags = $this->exec('git tag');
 		natsort($tags);
 		$tags = array_slice($tags, count($tags) - 2);
 
-		$previous = $tags[0];
-		$latest = $tags[1];
+		return [
+			'previous' => $tags[0],
+			'latest' => $tags[1]
+		];
+	}
 
+	public function execute(InputInterface $input, OutputInterface $output)
+	{
+		$remoteUrl = $this->exec('git config --get remote.origin.url');
+
+		$matches = [];
+		if ( !preg_match('/^git@github\.com\:(?P<owner>.+)\/(?P<repo>.+)\.git$/', $remoteUrl[0], $matches) ) {
+			throw new RuntimeException('Cannot find a remote git repository.');
+		}
+		$repoOwner = $matches['owner'];
+		$repository = $matches['repo'];
+
+		$client = new Client(['base_uri' => 'https://api.github.com/']);
+
+		$accessToken = $this->getAccessToken($client);
+
+		$tags = $this->getReleaseTags($input);
 		$command = sprintf(
 			'git --no-pager log --pretty=oneline --merges %s...%s | perl -n -e \'/Merge pull request #(\d+)/ && print "$1\n"\'',
-			$previous,
-			$latest
+			$tags['previous'],
+			$tags['latest']
 		);
 
 		$out = [];
 
-		$client = new Client(['base_uri' => 'https://api.github.com/']);
 		$pullRequests = $this->exec($command);
 		foreach ($pullRequests as $pullRequest) {
 			$url = sprintf(
 				'/repos/%s/%s/pulls/%s',
-				$matches['owner'],
-				$matches['repo'],
+				$repoOwner,
+				$repository,
 				$pullRequest
 			);
 
 			$response = $client->request('GET', $url, [
-			    'query' => ['access_token' => '65e85af1e06a8622374ca26a88415c96b56b945d']
+			    'query' => ['access_token' => $accessToken]
 			]);
-
 			$response = json_decode($response->getBody());
 
 			// $labels = [];
@@ -79,6 +122,5 @@ class GenerateChangelogCommand extends Command {
 		}
 
 		var_dump($out);
-		die();
 	}
 }
