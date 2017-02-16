1d97018833ea9d5fa6e0eab5c5047d034c189981
Justin Hayes <justin@soapboxhq.com>
2017-02-15 19:18:17 -0500

    Starting over

--- scripts/create-mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DB=$1;
-
-mysql -usoapbox -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
-mysql -usoapbox -psecret -e "CREATE DATABASE \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
